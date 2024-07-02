ee4c334d379b359ca315ce67f87fdc830f79aea8
Kanchan Pawar <kanchu9696@gmail.com>
2024-07-01 18:06:31 -0700

    addfield

---
+++ ClassAPI/MODEL/LoginModel.cs
@@ -0,0 +1,54 @@
+﻿using ClassAPI.BASE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAPI.MODEL
+{
+    public class LoginModel:DataEntity
+    {
+
+        [DisplayName("SrNo")]
+        [Key,Column("SrNo")]
+        public int srno { get; set; }
+
+        [DisplayName("UserId")]
+        [Column("UserId")]
+        public string userid { get; set; }
+
+
+        [DisplayName("Password")]
+        [Column("Password")]
+        public string password { get; set; }
+
+
+
+        [DisplayName("CustName")]
+        [Column("custname")]
+        public string custname { get; set; }
+
+        [DisplayName("Address")]
+        [Column("Address")]
+        public string address { get; set; }
+
+        [DisplayName("Salary")]
+        [Column("Salary")]
+        public decimal salary { get; set; }
+
+        [DisplayName("Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("SubCust")]
+        [Column("SubCust")]
+        public string SubCust { get; set; }
+
+    }
+ 
+
+}
